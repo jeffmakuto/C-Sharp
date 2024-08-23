f01665e4800d31e4c0c60361defc7542d81447a0
jeffmakuto <hjeffmakuto@gmail.com>
2024-08-23 07:05:34 -0700

    feat: create custom made boilerplate code for calling asyncronous operations in Main

---
+++ AsyncMain/Program.cs
@@ -0,0 +1,24 @@
+﻿class AsyncMain()
+{
+	public static int Main()
+	{
+		/* 
+		 * Call the async method and wait syncronously for it to
+		 * complete.
+		 * GetAwaiter().GetResult() is used to block the main thread
+		 * until the asyncronous operation completes
+		 */
+		return AsyncConsoleWork().GetAwaiter().GetResult();
+	}
+
+	// Async method that returns a Task<int>
+	private static async Task<int> AsyncConsoleWork()
+	{
+		/*
+		 * Main body of the async method
+		 * Perform asynchronous operations here
+		 * Returning 0 as the result of the task
+		 */
+		return 0;
+	}
+}
