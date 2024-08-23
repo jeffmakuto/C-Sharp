dece63a42989703e6d7bd750bfb16838708eecf2
jeffmakuto <hjeffmakuto@gmail.com>
2024-08-23 01:34:32 -0700

    feat: add subtraction, multiplication, and division operations to Program.cs

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -2,3 +2,15 @@
 int b = 6;
 int c = a + b;
 Console.WriteLine(c);
+
+// subtraction
+c = a - b;
+Console.WriteLine(c);
+
+// multiplication
+c = a * b;
+Console.WriteLine(c);
+
+// division
+c = a / b;
+Console.WriteLine(c);
