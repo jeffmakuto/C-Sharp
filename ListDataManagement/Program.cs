5c5b4a240ff235dec237611df007c29090c29c1d
jeffmakuto <hjeffmakuto@gmail.com>
2024-08-23 04:02:09 -0700

    feat: use Sort() method to sort the names

--- ListDataManagement/Program.cs
+++ ListDataManagement/Program.cs
@@ -37,3 +37,9 @@ else
     Console.WriteLine($"The name {names[index]} is at index {index}");
 
 }
+
+names.Sort();
+foreach (var name in names)
+{
+	Console.WriteLine($"Hello {name.ToUpper()}!");
+}
--- ListDataManagement/obj/Debug/net9.0/ListDataManagement.AssemblyInfo.cs
+++ ListDataManagement/obj/Debug/net9.0/ListDataManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListDataManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d339bc2ef37db34253541808d0cb9a117fb1ba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea5ee0dadd7e96ac1547032b31e76d69dccfb570")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListDataManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListDataManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
