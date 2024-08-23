00ea364def7d2559e1226085f91b08d1af2fbbf1
jeffmakuto <hjeffmakuto@gmail.com>
2024-08-23 03:53:44 -0700

    feat: check the list using indices

--- ListDataManagement/Program.cs
+++ ListDataManagement/Program.cs
@@ -12,3 +12,6 @@ foreach (var name in names)
 {
 	Console.WriteLine($"Hello {name.ToUpper()}!");
 }
+
+Console.WriteLine($"My name is {names[0]}");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
