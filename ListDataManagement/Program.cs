418aa9113b946c13ed6940dda58f745599217167
jeffmakuto <hjeffmakuto@gmail.com>
2024-08-23 03:51:29 -0700

    feat: Add additional names

--- ListDataManagement/Program.cs
+++ ListDataManagement/Program.cs
@@ -1,5 +1,14 @@
 ﻿List<string> names = ["Jeff", "Joy", "Nikita"];
 foreach (var name in names)
 {
-    Console.WriteLine($"Hello {name.ToUpper()}!");
+	Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+	Console.WriteLine($"Hello {name.ToUpper()}!");
 }
--- ListDataManagement/obj/Debug/net9.0/ListDataManagement.AssemblyInfo.cs
+++ ListDataManagement/obj/Debug/net9.0/ListDataManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListDataManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3538d0cf25ace2919b1765af811bb5f33a0918a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+268fa9a68a14488b7d3ea56e1ac524adc7d6b4c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListDataManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListDataManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
