4944e334138d4730a5c863ec24f2c74e3647f70b
jeffmakuto <hjeffmakuto@gmail.com>
2024-08-23 01:53:45 -0700

    feat: implement WorkWithIntegers method for basic arithmetic operations

--- NumbersInCSharp/Program.cs
+++ NumbersInCSharp/Program.cs
@@ -1,17 +1,22 @@
-﻿//Addition
-int a = 18;
-int b = 6;
-int c = a + b;
-Console.WriteLine(c);
+﻿WorkWithIntegers();
 
-// subtraction
-c = a - b;
-Console.WriteLine(c);
+void WorkWithIntegers()
+{
+	// Addition
+	int a = 18;
+	int b = 6;
+	int c = a + b;
+	Console.WriteLine(c);
 
-// multiplication
-c = a * b;
-Console.WriteLine(c);
+	// subtraction
+	c = a - b;
+	Console.WriteLine(c);
 
-// division
-c = a / b;
-Console.WriteLine(c);
+	// multiplication
+	c = a * b;
+	Console.WriteLine(c);
+
+	// division
+	c = a / b;
+	Console.WriteLine(c);
+}
